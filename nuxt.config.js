f63da822dd59b1b36cf56283062ec26b8483da17
bryanchua777 <bryanchua5327@gmail.com>
2021-01-28 19:58:50 +0800

    no message

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,8 +10,8 @@ export default {
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    titleTemplate: '%s - mypage',
-    title: 'mypage',
+    titleTemplate: '%s',
+    title: 'Bryan Chua',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
